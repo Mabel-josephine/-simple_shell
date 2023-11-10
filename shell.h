3f3a768e53eaaf4c75e94aa04844ab90d8acc233
Mabel-josephine <kuukuabruce55@gmail.com>
2023-11-10 04:44:26 -0800

    shell.h

---
+++ shell.h
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+ int main()
+ { 
+         char command[100]; 
+	 pid_t pid;
+while (1)
+ { 
+write(STDOUT_FILENO, "please enter a command:", 
+sizeof("Please enter a command:") -1);
+ fflush(stdout); 
+   /*Handle Ctrl+D (end of file)*/
+
+if (fgets(command, sizeof(command),stdin) == NULL)
+ { 
+write(STDOUT_FILENO, "\nExiting Simple Shell...\n", 
+sizeof("\nExiting Simple Shell...\n") -1); 
+break; 
+} 
+
+   /*Remove the newline character from the end of the command*/
+   
+ pid = fork();
+
+ if (pid == -1) 
+{ 
+
+/*execute the command in the child process*/
+perror("Unable to start new process"); exit(1);
+ }
+
+ }
+
+ return (0); 
+}
+
