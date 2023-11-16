28d5f70faf628b314c1d5e4b3c34467cac8c84a1
Mabel-josephine <kuukuabruce55@gmail.com>
2023-11-16 09:14:24 -0800

    shell.c

--- shell.c
+++ shell.c
@@ -2,53 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 
+#define MAX_INPUT_SIZE 1024 /* maximum input size*/
 
- int main(void)
- {
-         char command[100];
-         pid_t pid;
-while (1)
- {
-write(STDOUT_FILENO, "please enter a command:",
-sizeof("Please enter a command:") -1);
- fflush(stdout);
-   /*Handle Ctrl+D (end of file)*/
-
-if (fgets(command, sizeof(command),stdin) == NULL)
- {
-write(STDOUT_FILENO, "\nExiting Simple Shell...\n",
-sizeof("\nExiting Simple Shell...\n") -1);
-break;
+void executeCommand(char *command) {
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        /* The child process */
+        char *args[] = {command, NULL};
+        if (execvp(args[0], args) == -1) {
+            perror("execvp");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        /* The parent process */
+        wait(NULL);
+    }
 }
 
-   /*Remove the newline character from the end of the command*/
+int main(void) {
+    char command[] = "/bin/ls";
 
- pid = fork();
+    /* Execute the command */
+    executeCommand(command);
 
- if (pid == -1)
-{
+    return 0;
+}
 
-/*execute the command in the child process*/
-
-
-
-
-perror("Unable to start new process"); exit(1);
- }
-
- }
-
- int main(void){
-	 char command[] = "/bin/ls";
-
-	 /*command to execute*/
-	 executeCommand(command);
-
-	 return 0;
- }
 
 
 
